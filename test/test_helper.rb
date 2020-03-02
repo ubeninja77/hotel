528071cdc6969308c3c1f9effdc37de94a4f9925
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-02 15:55:56 -0800

    git commit testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,4 +5,21 @@ require "minitest/reporters"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# require_relative your lib files here!
+require 'simplecov'
+SimpleCov.start
+# Require_relative your lib files here!
+require_relative '../lib/room'
+# require_relative '../lib/reservation'
+# require_relative '../lib/date_range'
+# require_relative '../lib/hotel'
+
+
+describe "Room instantiation" do
+  before do
+    @reservation = Hotel::Room.new
+  end
+
+  it "is an instance of Room" do
+    expect(@room_availability).must_be_kind_of Hotel::Room
+  end
+end
