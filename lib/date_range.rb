de27498e957e7463ec60589d8ec2b7aa9a7e47e0
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-02 15:54:49 -0800

    testing git commit

---
+++ lib/date_range.rb
@@ -0,0 +1,31 @@
+# require_relative 
+# require_relative 
+
+# module Hotel
+#   class DateRange
+
+#   attr_reader 
+#   attr_writer 
+#   attr_accessor 
+
+
+
+#   def initialize
+
+
+
+
+
+#   end
+
+
+
+
+
+
+
+# end
+
+
+  
+# end
---
+++ lib/reservation.rb
@@ -0,0 +1,33 @@
+
+
+# module Hotel
+# class Reservation
+
+#   attr_reader :start_date, :end_date, :room_number, :total_cost
+
+
+#   def initialize (start_date, end_date, room_number, total_cost)
+#     @start_date = start_date
+#     @end_date = end_date
+#     @room_number = room_number 
+#     @total_cost = total_cost
+
+#   end
+
+#   end
+
+#   # I can access the list of all of the rooms in the hotel
+#   def room_access()
+# 
+
+#   # total cost of the stay
+#   def total_cost
+
+#     return total_cost
+#   end
+
+
+
+
+
+end
