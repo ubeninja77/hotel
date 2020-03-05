91009bd50198f29eb14de2463e4f8499fff9000c
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-05 12:25:37 -0800

    updated start/end date in initialize

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,31 +1,36 @@
-# require_relative 
-# require_relative 
+require_relative "hotel_controller"
 
-# module Hotel
-#   class DateRange
+module Hotel
+  class DateRange
+    attr_accessor :start_date, :end_date
 
-#   attr_reader 
-#   attr_writer 
-#   attr_accessor 
+    def initialize(start_date, end_date)
+      @start_date = Date.parse(start_date) #string?
+      @end_date = Date.parse(end_date)
+        if @end_date <= @start_date
+          raise ArgumentError.new "Invalid date. Your start date cannot be after your end date."  
+        end
+    end
 
+    def overlap?(start_date,end_date)
+      if start_date <= end_date && start_date >= end_date #parse through dates and check if output is str
+        return false 
+      end
+    end
 
+    def include?(date) #
+      # if date == start_date raise ArgumentError "Sorry, that is not a vaild date." #arg error needed?
+      if date >= start_date && date <= end_date
+        return true
+      else 
+        return false 
+      end
+    end
 
-#   def initialize
-
-
-
-
-
-#   end
-
-
-
-
-
-
-
-# end
-
-
-  
-# end
+    def nights
+      # pulls from the 
+      # need to return the number of nights per ressie
+      return 3
+    end
+  end
+end
