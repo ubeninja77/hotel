d8b5910698e10d29f8ae57dd48952f973c041200
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-04 23:04:37 -0800

    added helper method, initialized method, rooms method

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -1,34 +1,41 @@
 module Hotel
   class HotelController
 
-    attr_reader :guest_id, :room_num, :start_date, :end_date
+    attr_accessor :start_date, :end_date, :room_number
+
+    def initialize
+      @start_date = start_date
+      @end_date = end_date
+      @room_number = room_number
+    end
 
     # Wave 1
     def rooms 
       hotel_rooms = [] # empty array of hotel room numbers
       room_number = 1 # start of the hotel room numbers
-      while room_number <= 20 do # while the number of hotel rooms are less than 20, a loop runs
+      while room_number <= 20 do # the number of hotel rooms are less than 20, a loop runs
         room_number += 1 # this adds to the room number, by increments of 1
         hotel_rooms << room_number # shovels the room number into the hotel_rooms array
       end 
         return hotel_rooms 
     end
 
-  #   def reserve_room(start_date, end_date)
-  #     # start_date and end_date should be instances of class Date
-  #     rooms = find_rooms(room_num)
-  #     return Reservation.new(start_date, end_date)
-  #   end
+    def reserve_room(start_date, end_date)
+      # start_date and end_date should be instances of class Date
+      # rooms = find_rooms(room_number)
+      reservation = Hotel::Reservation.new(start_date, end_date, room_number)
+      return Reservation.new(start_date, end_date, room_number)
+    end
 
-  #   def reservations(date)
+    def reservations(date)
+    # need to access ressie by date (iterate through array?)
+      return []
+    end
 
-  #     return []
-  #   end
-
-  #   # Wave 2
-  #   def available_rooms(start_date, end_date)
-  #     # start_date and end_date should be instances of class Date
-  #     return []
-  #   end
+    # Wave 2
+    def available_rooms(date)
+      # start_date and end_date should be instances of class Date
+      return []
+    end
   end
 end
