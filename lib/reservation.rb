61aff244b7c64d2cdc774054557b6d49ed2d7f8d
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-04 23:02:23 -0800

    added helper method and added initialize method'

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,11 +1,21 @@
 module Hotel
   class Reservation
+
+    attr_reader :guest_id, :room_num, :start_date, :end_date
+
     # Feel free to change this method signature as needed. Make sure to update the tests!
-    def initialize(start_date, end_date, room)
+    def initialize(start_date:, end_date:, room_num:)
+      @start_date = start_date
+      @end_date = end_date
+      @room_number = room_number
+      @rate = rate
     end
 
+    # calculates the total of each ressie
     def cost
-      return 3
+    # access each reservation
+    # add the total dates (access array?)
+      return 3 #returns sum of ressie
     end
   end
 end
