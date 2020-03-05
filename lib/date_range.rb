c2fd43e9f9e61c550af55708ec8964fa4734c743
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-05 13:01:07 -0800

    updated initialize method end/start date

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -3,8 +3,8 @@ module Hotel
     attr_accessor :start_date, :end_date
 
     def initialize(start_date, end_date)
-      @start_date = Date.parse(start_date) #string?
-      @end_date = Date.parse(end_date)
+      @start_date = start_date
+      @end_date = end_date
         if @end_date <= @start_date
           raise ArgumentError "Invalid date. Your start date cannot be after your end date."  
         end
