a471fb7b386968f1282413b384c2a6fe97ffeb66
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-07 23:38:19 -0800

    rewrote rooms and reserve rooms method. updating avail rooms and res_by_date method to answer wave 2 questions.

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -1,41 +1,82 @@
 module Hotel
   class HotelController
 
-    attr_accessor :start_date, :end_date, :room_number
+    attr_accessor :start_date, :end_date, :room_number, :reservations
 
     def initialize
       @start_date = start_date
       @end_date = end_date
       @room_number = room_number
+      @reservations = []
+      rooms
     end
 
     # Wave 1
     def rooms 
-      hotel_rooms = [] # empty array of hotel room numbers
-      room_number = 1 # start of the hotel room numbers
-      while room_number <= 20 do # the number of hotel rooms are less than 20, a loop runs
-        room_number += 1 # this adds to the room number, by increments of 1
-        hotel_rooms << room_number # shovels the room number into the hotel_rooms array
+      @hotel_rooms = [] 
+      room_number = 0
+      while room_number < 20 do 
+        room_number += 1 
+        @hotel_rooms << room_number 
       end 
-        return hotel_rooms 
+        return @hotel_rooms 
     end
 
-    def reserve_room(start_date, end_date)
-      # start_date and end_date should be instances of class Date
-      # rooms = find_rooms(room_number)
-      reservation = Hotel::Reservation.new(start_date: start_date,end_date: end_date,room_num: room_number)
+    def verify_available(hotel_rooms, date)
+      if room_available == true #true if rooms is available
+        return 
+      end
+    end
+
+    # lists avialable rooms
+    # def open_rooms()
+    #   available_room = [] #array of available rooms
+    #   @reservations.each do |room| # iterates through the array of rooms
+    #   end
+    # end
+
+    # reserves a room aka creates a reservation
+    # takes two Date objects and returns a Reservation ex: (reservation).must_be_kind_of Hotel::Reservation
+    def reserve_room(start_date, end_date, room_number, rate) 
+      reservation = Hotel::Reservation.new(start_date, end_date, room_number, rate)
+      @reservations << reservation
       return reservation
     end
 
-    def reservations(date)
-    # need to access ressie by date (iterate through array?)
-      return []
+    # takes a Date and returns an array of Reservations by date and the 
+    def reservations_by_date(date)
+
+      []
     end
 
     # Wave 2
-    def available_rooms(date)
-      # start_date and end_date should be instances of class Date
-      return []
+    # locate avaialable rooms by two dates and returns an array
+    def available_rooms(start_date, end_date)
+      available_room = @hotel_rooms 
+
+        if reservation.include?(start_date == start_date, end_date = end_date)
+          raise ArgumentError "Sorry, there is no room available on those days."
+        else
+        # need to compare the start/end date to current reservations
+
+
+        # return an array 
+        end
+      
+      end
+  
+
+    #   @hotel_rooms.each do |room_num|
+    #     if room_num.available(start_date, end_date) == true
+    #     available_room << room_num
+    #     end
+    #     if available_room.length == 0
+    #       raise DateError.new("There are no rooms open for that date")
+    #     else
+    #       return available_room
+    #     end
+      # end
+    #   # start_date and end_date should be instances of class Date
     end
   end
 end
