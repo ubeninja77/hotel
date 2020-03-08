0c7322c6004f4162a117416ea28d6016c73eebd4
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-07 23:35:44 -0800

    updated nights method

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -9,13 +9,15 @@ module Hotel
           raise DateError "Invalid date. Your start date cannot be after your end date."  
         end
     end
- 
-    def overlap?(date_range)
+    
+    # checks if a reservation overlaps with another
+    def overlap?(date_range) #need to verify date_range is defined
       if start_date <= end_date && end_date >= start_date
-        return true
+        return tru
       end
     end
 
+    # checks if date is included in a reservation
     def include?(date) 
       if date >= start_date && date <= end_date
         return true
@@ -23,7 +25,8 @@ module Hotel
         return false 
       end
     end
-
+    
+    # find the total of nights stayed
     def nights
       length_of_stay = (@end_date - @start_date).to_i
       return length_of_stay
