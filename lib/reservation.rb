6eb8dfb8ea8f2f19b5e9fc83b15574b558c15e3f
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-07 23:34:11 -0800

    updated cost method (rate)

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,21 +1,22 @@
 module Hotel
   class Reservation
 
-    attr_reader :guest_id, :room_num, :start_date, :end_date
+    attr_reader :guest_id, :room_number, :start_date, :end_date, :rate, :reservations
+
+    # ROOM_RATE = 200
 
     # Feel free to change this method signature as needed. Make sure to update the tests!
-    def initialize(start_date:, end_date:, room_num:)
+    def initialize(start_date, end_date, room_number, rate = 200)
       @start_date = start_date
       @end_date = end_date
-      @room_number = room_number
+      @room_number = room_number 
       @rate = rate
     end
 
-    # calculates the total of each ressie
+    # calculates the total of each reservation
     def cost
-    # access each reservation
-    # add the total dates (access array?)
-      return 3 #returns sum of ressie
+      total_cost = (@end_date - @start_date).to_i * rate
+      return total_cost
     end
   end
 end
