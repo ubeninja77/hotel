ae9df6420fb3720bf13c41f42104ddb11c304393
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-08 21:32:20 -0700

    cleaned up the attr_reader and added date_range as a instance variable

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,22 +1,26 @@
 module Hotel
   class Reservation
 
-    attr_reader :guest_id, :room_number, :start_date, :end_date, :rate, :reservations
+    attr_reader :guest_id, :room_number, :rate, :date_range
 
     # ROOM_RATE = 200
 
     # Feel free to change this method signature as needed. Make sure to update the tests!
     def initialize(start_date, end_date, room_number, rate = 200)
-      @start_date = start_date
-      @end_date = end_date
+      @date_range = DateRange.new(start_date, end_date)
       @room_number = room_number 
       @rate = rate
     end
 
     # calculates the total of each reservation
     def cost
-      total_cost = (@end_date - @start_date).to_i * rate
+      total_cost = @date_range.nights.to_i * rate
       return total_cost
+    end
+    # 
+    def available?(start_date, end_date)
+      # This  fails if end date is afer the end date in date_range but start date is within range; returns false
+      return !(@date_range.include?(start_date) && @date_range.include?(end_date))
     end
   end
 end
