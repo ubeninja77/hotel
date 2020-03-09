e3c4ea42786af4ae95c3c81a04b3b31207193fe5
Lola Llanes <mslourdesllanes@gmail.com>
2020-03-09 10:34:46 -0700

    passed wave 1 and 2

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -5,7 +5,9 @@ describe Hotel::Reservation do
     it "returns a number" do
       start_date = Date.new(2017, 01, 01)
       end_date = start_date + 3
-      reservation = Hotel::Reservation.new(start_date, end_date, nil)
+      room_number = 7
+      rate = 200
+      reservation = Hotel::Reservation.new(start_date, end_date, room_number, rate)
       expect(reservation.cost).must_be_kind_of Numeric
     end
   end
